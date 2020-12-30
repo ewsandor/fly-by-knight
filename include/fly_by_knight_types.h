ab75bb755eac2ab0451dcabe61f70d70a6a44b73
Ed Sandor <ed@ewsandor.com>
2020-12-30 14:42:00 -0700

    Reorganize and boostrap cmake

---
+++ include/fly_by_knight_types.h
@@ -0,0 +1,12 @@
+/*
+ fly_by_knight.h
+ Fly by Knight - Chess Engine
+ Edward Sandor
+ December 2020
+ 
+ Common types for Fly by Knight
+*/
+
+//Include Farewell to King library
+#include <farewell_to_king.h>
+#include <farewell_to_king_types.h>
---
+++ src/fly_by_knight.c
@@ -0,0 +1,88 @@
+/*
+ fly_by_knight.c
+ Fly by Knight - Chess Engine
+ Edward Sandor
+ December 2020
+ 
+ Main file for Fly by Knight
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <farewell_to_king_strings.h>
+
+#include "fly_by_knight_types.h"
+
+int main(){
+  ftk_game_s game;
+  ftk_game_end_e game_end;
+  ftk_begin_standard_game(&game);
+
+  char input[128];
+  char out[1024];
+  ftk_move_s move = {0};
+
+  for(;;){
+    //printBoard(game.board, out);
+    ftk_board_to_string_with_coordinates(&game.board, out);
+    printf("%s\r\n", out);
+
+    game_end = ftk_check_for_game_end(&game);
+    if(FTK_END_NOT_OVER == game_end)
+    {
+      if(FTK_CHECK_IN_CHECK == ftk_check_for_check(&game))
+      {
+        printf("CHECK!\r\n\r\n");
+      }
+    }
+    else if (FTK_END_CHECKMATE == game_end) 
+    {
+      printf("CHECKMATE!\r\n\r\n");
+    }
+    else if (FTK_END_DRAW_STALEMATE == game_end) 
+    {
+      printf("STALEMATE!\r\n\r\n");
+    }
+    else {
+      printf("GAME OVER! Reason %u\r\n\r\n", game_end);
+    }
+
+    ftk_game_to_fen_string(&game, out);
+    printf("%s\r\n", out);
+
+    scanf("%s", input);
+
+    if(strcmp("q",input) == 0 || strcmp("quit",input)==0)
+        break;
+
+    if(strcmp("u",input) == 0)
+    {
+      ftk_move_backward(&game, &move);
+    }
+    else if(strcmp("r",input) == 0)
+    {
+      ftk_move_forward(&game, &move);
+    }
+    else if(strcmp("n",input) == 0)
+    {
+      ftk_begin_standard_game(&game);
+    }
+    else 
+    {
+      ftk_position_t target = FTK_XX;
+      ftk_position_t source = FTK_XX;
+
+      ftk_type_e   pawn_promo_type = FTK_TYPE_EMPTY;
+      ftk_castle_e castle_type     = FTK_CASTLE_NONE;
+      ftk_xboard_move(input, &target, &source, &pawn_promo_type, &castle_type);
+
+      move = ftk_move_piece(&game, target, source, pawn_promo_type);
+
+      ftk_move_backward(&game, &move);
+      printf("%d\n\n", ftk_move_forward(&game, &move));
+    }
+  }
+
+  return 0;
+}
