a5d80d7d263decba18cb8d557fb9dcb412228794
Ed Sandor <ed@ewsandor.com>
2021-01-10 16:05:10 -0700

    Basic capture weighting

--- include/fly_by_knight_algorithm_constants.h
+++ include/fly_by_knight_algorithm_constants.h
@@ -19,7 +19,7 @@
 #define FBK_SCORE_KING   1000000
 
 /* Max score for each color */
-#define FBK_SCORE_WHITE_MAX  (64  * FBK_SCORE_KING)
+#define FBK_SCORE_WHITE_MAX  ( 64 * FBK_SCORE_KING)
 #define FBK_SCORE_BLACK_MAX  (-64 * FBK_SCORE_KING)
 
 /* Weight per legal move for piece */
@@ -29,6 +29,23 @@
 #define FBK_SCORE_ROOK_MOVE    5
 #define FBK_SCORE_QUEEN_MOVE   9
 #define FBK_SCORE_KING_MOVE   10
+
+/* Weight potential capture */
+#define FBK_SCORE_CAPTURE_PAWN   (FBK_SCORE_PAWN/2)
+#define FBK_SCORE_CAPTURE_KNIGHT (FBK_SCORE_KNIGHT/2)
+#define FBK_SCORE_CAPTURE_BISHOP (FBK_SCORE_BISHOP/2)
+#define FBK_SCORE_CAPTURE_ROOK   (FBK_SCORE_ROOK/2)
+#define FBK_SCORE_CAPTURE_QUEEN  (FBK_SCORE_QUEEN/2)
+#define FBK_SCORE_CAPTURE_KING   FBK_SCORE_PAWN
+
+/* Weight potential loss */
+#define FBK_SCORE_LOSS_PAWN   ((2*FBK_SCORE_PAWN)/3)
+#define FBK_SCORE_LOSS_KNIGHT ((2*FBK_SCORE_KNIGHT)/3)
+#define FBK_SCORE_LOSS_BISHOP ((2*FBK_SCORE_BISHOP)/3)
+#define FBK_SCORE_LOSS_ROOK   ((2*FBK_SCORE_ROOK)/3)
+#define FBK_SCORE_LOSS_QUEEN  ((2*FBK_SCORE_QUEEN)/3)
+#define FBK_SCORE_LOSS_KING   (2*FBK_SCORE_PAWN)
+
 
 /* Score for the ability to castle */
 #define FBK_SCORE_CAN_CASTLE (FBK_SCORE_PAWN/3)
--- src/fly_by_knight_analysis.c
+++ src/fly_by_knight_analysis.c
@@ -12,6 +12,118 @@
 #include "fly_by_knight_algorithm_constants.h"
 #include "fly_by_knight_analysis.h"
 
+fbk_score_t fbk_score_potential_capture_value(ftk_type_e piece_type)
+{
+  fbk_score_t score = 0;
+
+  switch (piece_type)
+  {
+    case FTK_TYPE_PAWN:
+    {
+      score = FBK_SCORE_CAPTURE_PAWN;
+      break;
+    }
+    case FTK_TYPE_KNIGHT:
+    {
+      score = FBK_SCORE_CAPTURE_KNIGHT;
+      break;
+    }
+    case FTK_TYPE_BISHOP:
+    {
+      score = FBK_SCORE_CAPTURE_BISHOP;
+      break;
+    }
+    case FTK_TYPE_ROOK:
+    {
+      score = FBK_SCORE_CAPTURE_ROOK;
+      break;
+    }
+    case FTK_TYPE_QUEEN:
+    {
+      score = FBK_SCORE_CAPTURE_QUEEN;
+      break;
+    }
+    case FTK_TYPE_KING:
+    {
+      score = FBK_SCORE_CAPTURE_KING;
+      break;
+    }
+    default:
+    {
+      break;
+    }
+  }
+
+  return score;
+}
+
+fbk_score_t fbk_score_potential_loss_value(ftk_type_e piece_type)
+{
+  fbk_score_t score = 0;
+
+  switch (piece_type)
+  {
+    case FTK_TYPE_PAWN:
+    {
+      score = FBK_SCORE_LOSS_PAWN;
+      break;
+    }
+    case FTK_TYPE_KNIGHT:
+    {
+      score = FBK_SCORE_LOSS_KNIGHT;
+      break;
+    }
+    case FTK_TYPE_BISHOP:
+    {
+      score = FBK_SCORE_LOSS_BISHOP;
+      break;
+    }
+    case FTK_TYPE_ROOK:
+    {
+      score = FBK_SCORE_LOSS_ROOK;
+      break;
+    }
+    case FTK_TYPE_QUEEN:
+    {
+      score = FBK_SCORE_LOSS_QUEEN;
+      break;
+    }
+    case FTK_TYPE_KING:
+    {
+      score = FBK_SCORE_LOSS_KING;
+      break;
+    }
+    default:
+    {
+      break;
+    }
+  }
+
+  return score;
+}
+
+fbk_score_t fbk_score_potential_capture(ftk_square_s square, ftk_color_e turn)
+{
+  fbk_score_t score;
+
+  if(turn == square.color)
+  {
+    score = fbk_score_potential_loss_value(square.type);
+  }
+  else
+  {
+    score = fbk_score_potential_capture_value(square.type);
+  }
+
+  if(FTK_COLOR_WHITE == square.color)
+  {
+    /* If capture square is white, black is moving */
+    score *= -1;
+  }
+
+  return score;
+}
+
 /**
  * @brief Score game position for white or black advantage
  * 
@@ -25,6 +137,8 @@ fbk_score_t fbk_score_game(const ftk_game_s * game)
   int         advantage;
 
   int legal_move_count;
+  ftk_board_mask_t capture_mask;
+  ftk_position_t   capture_square;
   unsigned int white_king_not_moved  = 0;
   unsigned int white_rooks_not_moved = 0;
   unsigned int black_king_not_moved  = 0;
@@ -34,6 +148,14 @@ fbk_score_t fbk_score_game(const ftk_game_s * game)
   {
     advantage = (FTK_COLOR_WHITE == game->board.square[i].color)?1:-1;
     legal_move_count = ftk_get_num_bits_set(game->board.move_mask[i]);
+
+    capture_mask = game->board.move_mask[i] & game->board.board_mask;
+    while(capture_mask)
+    { 
+      capture_square = ftk_get_first_set_bit_idx(capture_mask);
+      FTK_CLEAR_BIT(capture_mask, capture_square);
+      score += fbk_score_potential_capture(game->board.square[capture_square], game->turn);
+    }
 
     switch(game->board.square[i].type)
     {
